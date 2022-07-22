eaed721ed9f95d8738cc919f1d157b82e872a8bf
lost-sky <89430129+creeperCN@users.noreply.github.com>
2022-07-22 15:40:52 +0800

    Add files via upload

---
+++ Javascript/Test.js
@@ -0,0 +1,10 @@
+	var num=1;
+	function eventfuncion(){
+		if(document.getElementsByClassName("egg")){
+			num++;
+		}if(num>=11){
+			alert("已开启开发者模式");
+		}if(num>=12){
+			alert("不要再点了qwq");
+		}
+	}	
